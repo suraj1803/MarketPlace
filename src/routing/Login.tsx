f10f98ea839e4b5f029299a2ba41cadacdda1ac6
Suraj Biswas <surajbiswas182003@gmail.com>
2025-02-09 05:20:38 +0000

    fixed: the layout and form

--- src/routing/Login.tsx
+++ src/routing/Login.tsx
@@ -1,6 +1,5 @@
 import {
   Text,
-  Link,
   VStack,
   Field,
   Input,
@@ -8,9 +7,13 @@ import {
   Heading,
   Button,
 } from "@chakra-ui/react";
+
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link } from "react-router";
+
 import { PasswordInput } from "@/components/ui/password-input";
 import Navbar from "../components/NavBar";
-import { useNavigate } from "react-router";
+import { Form, useNavigate } from "react-router";
 import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -51,46 +54,52 @@ const Login = () => {
           <Heading mb={7} textStyle={"3xl"}>
             Welcome
           </Heading>
+
           <Field.Root>
-            <Field.Label fontSize="lg" mb="2">
-              Email
-            </Field.Label>
-            <Input
-              {...register("email")}
-              id="email"
-              placeholder="me@example.com"
-              size="lg"
-            />
-            {errors.email && (
-              <Text color={"red"} fontSize="sm">
-                {errors.email.message}
-              </Text>
-            )}
-            <Field.Label fontSize="lg" mb="2">
-              Password
-            </Field.Label>
-            <PasswordInput {...register("password")} id="password" size="lg" />
-            {errors.password && (
-              <Text color={"red"} fontSize="sm">
-                {errors.password.message}
-              </Text>
-            )}
-            <Button
-              type="submit"
-              fontSize={"md"}
-              mt={7}
-              paddingX={7}
-              width={"100%"}
-              onClick={handleSubmit(handleLogin)}
-            >
-              Login
-            </Button>
+            <Form onSubmit={handleSubmit(handleLogin)}>
+              <Field.Label fontSize="lg" mb="2">
+                Email
+              </Field.Label>
+              <Input
+                {...register("email")}
+                id="email"
+                placeholder="me@example.com"
+                size="lg"
+              />
+              {errors.email && (
+                <Text color={"red"} fontSize="sm">
+                  {errors.email.message}
+                </Text>
+              )}
+              <Field.Label fontSize="lg" mb="2">
+                Password
+              </Field.Label>
+              <PasswordInput
+                {...register("password")}
+                id="password"
+                size="lg"
+              />
+              {errors.password && (
+                <Text color={"red"} fontSize="sm">
+                  {errors.password.message}
+                </Text>
+              )}
+              <Button
+                type="submit"
+                fontSize={"md"}
+                mt={7}
+                paddingX={7}
+                width={"100%"}
+              >
+                Login
+              </Button>
+            </Form>
           </Field.Root>
           <Text fontSize={"14px"} mt="5">
             Not a member?{" "}
-            <Link variant="underline" href="/signup" colorPalette="teal">
-              Sign up now
-            </Link>
+            <Text as={"u"} color={"teal"} ml={"7px"}>
+              <Link to="/signup">Sign up now</Link>
+            </Text>
           </Text>
         </VStack>
       </Flex>
--- src/routing/Signup.tsx
+++ src/routing/Signup.tsx
@@ -1,6 +1,5 @@
 import {
   Text,
-  Link,
   VStack,
   Field,
   Input,
@@ -13,6 +12,7 @@ import Navbar from "../components/NavBar";
 import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, Link } from "react-router";
 
 const schema = z.object({
   name: z
@@ -52,65 +52,67 @@ const Signup = () => {
           <Heading mb={7} textStyle={"3xl"}>
             Signup
           </Heading>
-          <Field.Root>
-            <Field.Label fontSize="lg" mb="2">
-              Name
-            </Field.Label>
-            <Input
-              {...register("name")}
-              id="name"
-              placeholder="John Doe"
-              size="lg"
-            />
-            {errors.name && (
-              <Text color={"red"} fontSize="sm">
-                {errors.name.message}
-              </Text>
-            )}
-            <Field.Label fontSize="lg" mb="2">
-              Email
-            </Field.Label>
-            <Input
-              {...register("email")}
-              id="email"
-              placeholder="johndoe@gmail.com"
-              size="lg"
-            />
-            {errors.email && (
-              <Text color={"red"} fontSize="sm">
-                {errors.email.message}
-              </Text>
-            )}
-            <Field.Label fontSize="lg" mb="2">
-              Password
-            </Field.Label>
-            <PasswordInput {...register("password")} id="password" size="lg" />
-            {errors.password && (
-              <Text color={"red"} fontSize="sm">
-                {errors.password.message}
-              </Text>
-            )}
 
-            <Button
-              onClick={handleSubmit(handleSignUp)}
-              fontSize={"md"}
-              mt={7}
-              paddingX={7}
-              width={"100%"}
-            >
-              SIGN UP
-            </Button>
+          <Field.Root>
+            <Form onSubmit={handleSubmit(handleSignUp)}>
+              <Field.Label fontSize="lg" mb="2">
+                Name
+              </Field.Label>
+              <Input
+                {...register("name")}
+                id="name"
+                placeholder="John Doe"
+                size="lg"
+              />
+              {errors.name && (
+                <Text color={"red"} fontSize="sm">
+                  {errors.name.message}
+                </Text>
+              )}
+              <Field.Label fontSize="lg" mb="2">
+                Email
+              </Field.Label>
+              <Input
+                {...register("email")}
+                id="email"
+                placeholder="johndoe@gmail.com"
+                size="lg"
+              />
+              {errors.email && (
+                <Text color={"red"} fontSize="sm">
+                  {errors.email.message}
+                </Text>
+              )}
+              <Field.Label fontSize="lg" mb="2">
+                Password
+              </Field.Label>
+              <PasswordInput
+                {...register("password")}
+                id="password"
+                size="lg"
+              />
+              {errors.password && (
+                <Text color={"red"} fontSize="sm">
+                  {errors.password.message}
+                </Text>
+              )}
+
+              <Button
+                type="submit"
+                fontSize={"md"}
+                mt={7}
+                paddingX={7}
+                width={"100%"}
+              >
+                SIGN UP
+              </Button>
+            </Form>
           </Field.Root>
           <Text fontSize={"14px"} mt="5">
-            Already have an account ?{" "}
-            <Link
-              paddingX={"3px"}
-              variant="underline"
-              href="/"
-              colorPalette="teal"
-            >
-              Login Now
-            </Link>
+            Already have an account ?
+            <Text as={"u"} color={"teal"} ml={"7px"}>
+              <Link to="/">Login now</Link>
+            </Text>
           </Text>
         </VStack>
       </Flex>
