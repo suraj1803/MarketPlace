c960d08cd4506de202cebad0c5d4a4c0db70ff9b
Suraj Biswas <surajbiswas182003@gmail.com>
2025-02-11 05:54:52 +0000

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router";
+import { Text } from "@chakra-ui/react";
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Text as={"u"} color={"teal"} ml={"7px"}>
+        <Link to="/login">Login</Link>
+      </Text>
+      <Text as={"u"} color={"teal"} ml={"7px"}>
+        <Link to="/signup">Signup</Link>
+      </Text>
+    </>
+  );
 };
 
 export default App;
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./Login";
 import Signup from "./Signup";
+import App from "@/App";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Login></Login> },
+  { path: "/", element: <App></App> },
   { path: "/login", element: <Login></Login> },
   { path: "/signup", element: <Signup></Signup> },
 ]);
